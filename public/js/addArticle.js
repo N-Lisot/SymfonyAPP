ca53b50995f57705c9358ced6bd1944c672d9afc
N-Lisot <nowaklisot@gmail.com>
2022-11-18 14:55:10 +0100

    add article

---
+++ public/js/addArticle.js
@@ -0,0 +1,11 @@
+var isCheak = 0;
+
+function cheak(){
+    if(isCheak == 0){
+        document.getElementById("").hidden = false;
+        isCheak = 1;
+
+    }else{
+        isCheak = 0;
+    }
+}
--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,11 +2,14 @@
 // src/Controller/Controller.php
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use App\Entity\User;
+use App\Entity\Article;
+
 
 class Controller extends AbstractController
 {
@@ -37,4 +40,48 @@ class Controller extends AbstractController
             'user' => $user,
         ]);
     }
+
+    /**
+     * @Route("/addArticle", name="addArticle")
+     */
+    public function addArticle(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+ 
+        $user = $this->getUser();
+
+        return $this->render('addArticle.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/addOneArticle", name="app_addArticle")
+     */
+    public function addOneArticle(ManagerRegistry $doctrine): Response
+    {
+        if(isset($_POST['cheakMedia']) && $_POST['cheakMedia'] == 'Yes') 
+        {
+            $media = true;
+        }else{
+            $media = false;
+        }
+        $entityManager = $doctrine->getManager();
+
+        $article = new Article();
+        $article->setTitre($_POST['titre']);
+        $article->setDescription($_POST['description']);
+        $article->setMedia($media);
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+
+        $user = $this->getUser();
+
+        return $this->render('addArticle.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
 }
